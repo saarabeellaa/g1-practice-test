3f6dcdba081213019874292bcd30c587a324bd87
saarabeellaa <saarabeellaa@gmail.com>
2026-01-11 21:59:54 -0500

    Replace BannerAd with AdMobBanner component

--- components/BannerAd.js
+++ components/BannerAd.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import { View } from 'react-native';
-import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+import { AdMobBanner } from 'expo-ads-admob';
 
-export function BannerAdComponent() {
-  return (
-    <View style={{
-      backgroundColor: '#f5f5f5',
-      alignItems: 'center',
-      paddingVertical: 5,
-    }}>
-      <BannerAd
-        unitId={__DEV__ ? TestIds.BANNER : 'ca-app-pub-3990448886293370/4801361884'}
-        size={BannerAdSize.SMART_BANNER}
-        requestOptions={{
-          requestNonPersonalizedAdsOnly: false,
-        }}
-      />
-    </View>
-  );
-}
+<AdMobBanner
+  bannerSize="smartBannerPortrait"
+  adUnitID="ca-app-pub-3940256099942544/6300978111"
+  servePersonalizedAds={false}
+/>
